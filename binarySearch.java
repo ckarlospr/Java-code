dd47b2722245d3199ec6c8d66e6d0d80f2b1f875
Carlos Perez <per.ckarlos@gmail.com>
2023-04-26 14:31:20 -0500

    binarySearch code done

--- binarySearch.java
+++ binarySearch.java
@@ -1,7 +1,30 @@
 public class BinarySearch {
 
     public static void main(String[] args) {
-        //cambio mas
-        //chanchito feliz
+        int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(binarySearch(data, 3));
+    }
+
+    public static int binarySearch(int[] data, int n){
+        
+        int start, middle, end, r;
+        start = 0;
+        end = data.length;
+        r=-1;
+
+        while(start < end){
+            middle = (start + end)/2;
+
+            if(n == data[middle]){
+                r=middle;
+                break;
+            }else if(n < data[middle]){
+                end = middle;
+            }else{
+                start = middle;
+            }
+        }
+
+        return r;
     }
 }
