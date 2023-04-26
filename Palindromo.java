da4d88a93c2b4b5492a1b0559b5ee9c9dfa173bb
Carlos Perez <per.ckarlos@gmail.com>
2023-04-26 14:43:40 -0500

    add palindomo code

--- Palindromo.java
+++ Palindromo.java
@@ -2,4 +2,18 @@ public class Palindromo {
     public static void main(String[] args) {
         //cambiuo
     }
+
+    public static void esPalindromo(String data){
+        int middle = data.length()/2;
+        String r = "Yes";
+
+        for(int i=0; i<=middle; i++){
+            if(data.charAt(i)!=data.charAt(data.length()-i-1)){
+                r = "No";
+                break;
+            }
+        }
+
+        System.out.println(r);
+    }
 }
