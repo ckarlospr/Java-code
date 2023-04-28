6062aa983b9c75258a9e6b058a05ee9947e6b7ce
Carlos Perez <per.ckarlos@gmail.com>
2023-04-28 16:53:31 -0500

    adding a chocolate excersice

---
+++ Excercise/BirthdayChocolate.java
@@ -0,0 +1,72 @@
+package Excercise;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class BirthdayChocolate {
+    /*
+     * Complete the 'birthday' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY s
+     *  2. INTEGER d
+     *  3. INTEGER m
+     */
+
+    public static int birthday(List<Integer> s, int d, int m) {
+    // Write your code here
+        int sum = 0;
+        int r = 0;
+        for (int i = 0; i <= s.size()-m; i++) {
+            for (int j = i; j < i+m ; j++) {
+                sum = sum + s.get(j);
+                System.out.print(""+s.get(j));
+            }
+            System.out.println("");
+            System.out.println(""+sum);
+            if(sum==d){
+                r++;
+            }
+            sum=0;
+        }
+        
+        return r;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] sTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> s = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int sItem = Integer.parseInt(sTemp[i]);
+            s.add(sItem);
+        }
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = BirthdayChocolate.birthday(s, d, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+}
